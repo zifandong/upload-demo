6f1bc746412253801804db655995b641f6f89853
dongzf <dongzf@lenovocloud.com>
2022-08-25 16:41:08 +0800

    feat: 测试

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,16 +1,18 @@
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore({
+const useCounterStore = defineStore({
   id: 'counter',
   state: () => ({
-    counter: 0
+    counter: 0,
   }),
   getters: {
-    doubleCount: (state) => state.counter * 2
+    doubleCount: state => state.counter * 2,
   },
   actions: {
     increment() {
+      // eslint-disable-next-line no-plusplus
       this.counter++
-    }
-  }
+    },
+  },
 })
+export default useCounterStore
